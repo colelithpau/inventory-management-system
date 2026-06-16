2be75c31c41da5250ec5466fd2ca2aeea690d23b
pinyinzi <jppaulo@student.apc.edu.ph>
2026-06-17 02:51:11 +0800

    Add Donation class for food item tracking

---
+++ classes.java
@@ -0,0 +1,11 @@
+public class Donation {
+    String foodItem;
+    int quantity;
+    String expiryDate;
+
+    public Donation(String foodItem, int quantity, String expiryDate) {
+        this.foodItem = foodItem;
+        this.quantity = quantity;
+        this.expiryDate = expiryDate;
+    }
+}
